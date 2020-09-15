6cfb176842eb5d24de5f5be699f79a4593a0e4d4
jzx <2329968598@qq.com>
2020-09-15 21:27:43 +0800

    项目骨架  v 1.0

---
+++ jzxcrowdfunding01-admin-parent/jzxcrowdfunding04-admin-entity/src/main/java/com/jzx/crowd/entity/Admin.java
@@ -0,0 +1,63 @@
+package com.jzx.crowd.entity;
+
+public class Admin {
+    private Integer id;
+
+    private String loginAcct;
+
+    private String userPassword;
+
+    private String userName;
+
+    private String email;
+
+    private String createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLoginAcct() {
+        return loginAcct;
+    }
+
+    public void setLoginAcct(String loginAcct) {
+        this.loginAcct = loginAcct == null ? null : loginAcct.trim();
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword == null ? null : userPassword.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+}
